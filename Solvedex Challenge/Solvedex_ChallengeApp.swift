9b20b46df2a4cac4a7190b2df1dfdc16ac98a8db
Datafox-JCP <datafox@outlook.com>
2024-01-23 13:40:13 -0600

    Initial Commit

---
+++ Solvedex Challenge/Solvedex_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solvedex_ChallengeApp.swift
+//  Solvedex Challenge
+//
+//  Created by Juan Hernandez Pazos on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Solvedex_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
